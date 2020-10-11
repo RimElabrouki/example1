9a83fe4ac278aef2ddd8099c0cc40302b8887542
RimElabrouki <72520179+RimElabrouki@users.noreply.github.com>
2020-10-11 23:16:03 +0200

    Update JavaApplication2.java

--- src/javaapplication2/JavaApplication2.java
+++ src/javaapplication2/JavaApplication2.java
@@ -17,7 +17,7 @@ public class JavaApplication2 {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        System.out.println("Hol  como estas!!");
+        System.out.println("Hola!!como estas!");
         
         
     }
